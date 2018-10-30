176b75fd3e09efb8e06aa551c6cb086543c2eba0
Jean Ribes <jean.ribes@insa-lyon.fr>
2018-10-30 19:43:25 +0100

    séparation vers une autre classe pour les Websocket

--- src/ChatWindow.java
+++ src/ChatWindow.java
@@ -1,122 +1,103 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
 import java.beans.PropertyChangeListener;
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.handshake.ServerHandshake;
-import java.net.URI;
 import java.net.URISyntaxException;
 
-public class ChatWindow extends JFrame {
+public class ChatWindow extends JFrame 
+implements ChatConnector.OnMessageListener,
+		   ChatConnector.OnReadyListener,
+		   WindowListener,
+		   ActionListener{
     String username;
     public static String message;
-    public WebSocketClient sync;
+    public ChatConnector chat;
     JTextField messageField;
     JButton sendButton;
     JTextArea history;
+    /*public static interface sendMessageInterface {
+		public void sendMessage(String message);
+		}
+	public static void sendMessage(String message){sendMessageInterface.sendMessage(message);}
+	*/
     public ChatWindow(String username) {
         this.username=username;
         System.out.println(username+" intializing");
-        try {setupWebSocket();}
+        try {chat = new ChatConnector(this);}
         catch(URISyntaxException e){e.printStackTrace();}
-        setLayout(new BorderLayout());
+        setLayout(new BorderLayout(0,0));
         setSize(500,600);
         setLocation(100,100);
         setTitle("Chat - "+username);
 
         messageField = new JTextField("votre message ici");
         //messageField.setBounds(3,50,230,30);
-        messageField.setSize(150,30);
-        messageField.addActionListener(new Action() {
-            public Object getValue(String key) {return null;}public void putValue(String key, Object value) {}public void setEnabled(boolean b) {}public boolean isEnabled() {return false;}public void addPropertyChangeListener(PropertyChangeListener listener) {}public void removePropertyChangeListener(PropertyChangeListener listener) {}
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                message = "["+username +"]> "+messageField.getText();
-                sync.send(message);
-                System.out.println(messageField.getText());
-                messageField.setText("");
-            }
-        });
+        //messageField.setSize(150,30);
+        messageField.addActionListener(this); //c'est pratique car toutes mes actions ont la même source
 
         sendButton = new JButton("Envoyer");
-        sendButton.setSize(100,30);
+        //sendButton.setSize(100,60);
         sendButton.setEnabled(false);
-        sendButton.setLocation(250,50);
-        sendButton.addActionListener(new Action() {
-            public Object getValue(String key) {return null;}public void putValue(String key, Object value) {}public void setEnabled(boolean b) {}public boolean isEnabled() {return false;}public void addPropertyChangeListener(PropertyChangeListener listener) {}public void removePropertyChangeListener(PropertyChangeListener listener) {}
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                message = "["+username +"]> "+messageField.getText();
-                sync.send(message);
-                System.out.println(messageField.getText());
-                messageField.setText("");
-            }
-        });
+        sendButton.addActionListener(this); //c'est pratique car toutes mes actions ont la même source
+        
         history = new JTextArea("historique:");
         JScrollPane scrollPane = new JScrollPane(history);
         JPanel scroll = new JPanel();
         scroll.setLayout(new BorderLayout());
-        scroll.setSize(500,500);
-        scroll.setBackground(Color.magenta);
-        //scroll.setLocation(3,100);
+        scroll.setBackground(Color.gray);
         scroll.add(scrollPane);
-        history.setBackground(Color.gray);
+        history.setBackground(Color.white);
         history.setEditable(false);
 		
 		JPanel haut = new JPanel();
-		JPanel bas = new JPanel();
-		haut.setLayout(new FlowLayout());
-		bas.setLayout(new FlowLayout());
+		GridLayout gl = new GridLayout(1,2);
+		gl.setHgap(20);
+		haut.setLayout(gl);
 		haut.setBackground(Color.blue);
-		bas.setBackground(Color.green);
-		haut.add(messageField);
-		haut.add(sendButton);
-		//bas.add(scroll);
+		haut.add(messageField,"1");
+		haut.add(sendButton,"2");
+		haut.setBorder(new EmptyBorder(10,10,10,10));
+		scroll.setBorder(new EmptyBorder(10,10,10,10));
 		add(haut, BorderLayout.NORTH);
 		add(scroll, BorderLayout.CENTER);
-        //add(scroll);
-        //add(sendButton);
-        //add(messageField);
-        
-        addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				message = username+" a quitté la discussion";
-				sync.send(message);
-				sync.close();
-			}
-		});
+       
         setVisible(true);
+        addWindowListener(this);
+        
     }
-    
-    public void setupWebSocket() throws URISyntaxException {
-		this.sync = new WebSocketClient(new URI("wss://api.ribes.me/tchat/room")) {
-            @Override
-            public void onOpen(ServerHandshake serverHandshake) {
-                System.out.println("opened connection");
-                sync.send(username+" a rejoint la discussion");
-                sendButton.setEnabled(true);
-            }
-
-            @Override
-            public void onMessage(String message) {
-                System.out.println(message);
-                history.append("\n"+message);
-            }
-
-            @Override
-            public void onClose(int code, String reason, boolean remote) {
-                System.out.println("closed connection");
-                System.out.println("Reason: " + reason + " code");
-                System.out.print(code);
-            }
-
-            @Override
-            public void onError(Exception ex) {
-                System.out.println("ERREUR WEBSOCKET");
-                ex.printStackTrace();
-            }
-        };
-        sync.connect(); //ne pas l'oublier ...
+    @Override
+	public void receiveMsg(String message) {
+		history.append("\n"+message);
 	}
+	@Override
+	public void getReady(){
+		sendButton.setEnabled(true);
+		send(username+" a rejoint la discussion");
+	}
+    public void send(String message) {
+		chat.send(message);
+	}
+	
+	@Override
+	public void windowClosing(WindowEvent e) {
+		message = username+" a quitté la discussion";
+		send(message);
+		chat.close();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		message = "["+username +"]> "+messageField.getText();
+		send(message);
+		System.out.println(messageField.getText());
+		messageField.setText("");
+	}
+	public void windowOpened(WindowEvent e) {}
+	public void windowActivated(WindowEvent e) {}
+	public void windowIconified(WindowEvent e) {}
+	public void windowDeiconified(WindowEvent e) {}
+	public void windowDeactivated(WindowEvent e) {}
+	public void windowClosed(WindowEvent e) {}
+	
+	public static void main(String[] args) {new ChatWindow("test username");}
 }
